ebafeb30b918243522fd0a61761d74a93ecba03c
victor rodrigues | tprime desktop <victor_duu@hotmail.com>
2026-03-25 16:14:59 -0300

    fix(backend): corrige o DTO de updatepaciente respeitando dados sensiveis que não devem poder mudar

--- backend/src/pacientes/dto/update-paciente.dto.ts
+++ backend/src/pacientes/dto/update-paciente.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePacienteDto } from './create-paciente.dto';
+import { IsDateString, IsEmail, IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdatePacienteDto extends PartialType(CreatePacienteDto) {}
+export class UpdatePacienteDto {
+    @ApiPropertyOptional({ example: '11999999999' })
+    @IsString()
+    @IsOptional()
+    telefone?: string;
+
+    @ApiPropertyOptional({ example: 'joao@email.com' })
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+}
